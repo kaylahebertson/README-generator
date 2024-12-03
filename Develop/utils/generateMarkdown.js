8c31113931faa4bfff3688a5fcbb4aa955d93060
Kayla Hebertson <kayla.hebertson@gmail.com>
2024-12-03 14:56:24 -0700

    add license badge

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,16 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license === 'MIT') {
+        return `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+    } else if (license === 'Apache') {
+        return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`;
+    } else if (license === 'GPL') {
+        return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`;
+    } else {
+        return '';
+    }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
