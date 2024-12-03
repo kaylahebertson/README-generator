20f92c6118360a70240c710362e724919fc3aacd
Kayla Hebertson <kayla.hebertson@gmail.com>
2024-12-03 14:43:15 -0700

    write file

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,7 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer';
+import fs from 'fs';
+import path from 'path';
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -55,7 +57,9 @@ const questions = [
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
