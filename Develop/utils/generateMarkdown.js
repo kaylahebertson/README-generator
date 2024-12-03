02fe33323f4dc5964a9a212597a64e4393b0a472
Kayla Hebertson <kayla.hebertson@gmail.com>
2024-12-03 15:05:25 -0700

    outline for readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -40,7 +40,25 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+${renderLicenseBadge(data.license)}
 
+## Description
+
+## Table of Contents
+
+*
+
+## Installation
+
+## Usage
+
+## License
+
+## Contributing
+
+## Tests
+
+## Questions
 `;
 }
 
