990ffbbc6db7845240307beef00d58a7113eb8be
Kayla Hebertson <kayla.hebertson@gmail.com>
2024-12-03 14:31:21 -0700

    basic questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,58 @@
 // TODO: Include packages needed for this application
-
+import inquirer from 'inquirer';
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Write a brief description of your project.',
+    },
+    // TODO: figure out how to add a table of contents
+    {
+        type: 'input',
+        name: 'table of contents',
+        message: '',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command is used to install dependencies?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How is your project meant to be used?',
+    },
+    // TODO: license choices
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Which license should your project have?',
+        choices: [''],
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: '',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What command is used to run tests?',
+    },
+    // TODO: github username, email should be added to questions
+    {
+        type: 'input',
+        name: 'questions',
+        message: '.',
+    }
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
